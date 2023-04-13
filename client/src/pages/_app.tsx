9cbb241d72766f95fb2fe30b1c9407f9467c00d9
celeron1ghz <celeron1ghz@gmail.com>
2023-04-13 10:15:00 +0900

    add emotion config

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import createEmotionCache from "../../src/theme/emotionCache";
+
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../theme/mytheme";
+
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function App({ Component, pageProps, emotionCache = clientSideEmotionCache }: MyAppProps) {
+  return <>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  </>
 }
